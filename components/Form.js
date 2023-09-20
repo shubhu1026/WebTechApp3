bb0e84dae7b9e9c26e0b427a5f1959e4fb116496
shubhu1026 <shubhu.p2000@gmail.com>
2023-09-20 12:26:06 -0400

    Labels and clear input functionality

--- components/Form.js
+++ components/Form.js
@@ -1,15 +1,52 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, TextInput } from "react-native";
+import React from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import Label from "./Label";
 
 export default function Form() {
+  const [firstName, setFirstName] = React.useState("");
+  const [lastName, setLastName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+
+  const ClearDetails = () => {
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>First Name</Text>
-      <TextInput placeholder="enter first name"></TextInput>
-      <Text>Last Name</Text>
-      <TextInput placeholder="enter last name"></TextInput>
-      <Text>Email</Text>
-      <TextInput placeholder="enter email"></TextInput>
+    <View>
+      <View style={styles.labelContainer}>
+        <Text style={styles.text}>First Name</Text>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Enter First Name"
+          onChangeText={(text) => setFirstName(text)}
+          value={firstName}
+        ></TextInput>
+      </View>
+
+      <View style={styles.labelContainer}>
+        <Text style={styles.text}>Last Name</Text>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Enter Last Name"
+          onChangeText={(text) => setLastName(text)}
+          value={lastName}
+        ></TextInput>
+      </View>
+
+      <View style={styles.labelContainer}>
+        <Text style={styles.text}>Email</Text>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Enter Email"
+          onChangeText={(text) => setEmail(text)}
+          value={email}
+        ></TextInput>
+      </View>
+
+      <Button title="Submit"></Button>
+      <Button onPress={ClearDetails} title="Clear"></Button>
     </View>
   );
 }
@@ -20,5 +57,36 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+
+  input: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+
+  label: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "blue",
+  },
+
+  labelContainer: {
+    width: "auto",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    marginHorizontal: 20,
+    marginVertical: 5,
+  },
+  text: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textInput: {
+    padding: 10,
+    marginLeft: 10,
+    width: "auto",
   },
 });
