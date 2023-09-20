bfaa5157b2d9c3b2777b3f533a84ad06b0bc834f
shubhu1026 <shubhu.p2000@gmail.com>
2023-09-20 10:58:48 -0400

    basic form text inputs

--- components/Form.js
+++ components/Form.js
@@ -1,11 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, TextInput } from "react-native";
-import Form from "./components/Form";
 
-export default function App() {
+export default function Form() {
   return (
     <View style={styles.container}>
-      <Form></Form>
+      <Text>First Name</Text>
+      <TextInput placeholder="enter first name"></TextInput>
+      <Text>Last Name</Text>
+      <TextInput placeholder="enter last name"></TextInput>
+      <Text>Email</Text>
+      <TextInput placeholder="enter email"></TextInput>
     </View>
   );
 }
